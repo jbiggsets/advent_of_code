bd5c2f4711f965a9d224c3b9d7e796ba0c1df821
jbiggsets <jeremy.m.biggs@gmail.com>
2020-12-03 16:57:56 -0500

    fix tabs in day3

--- day3/day3.py
+++ day3/day3.py
@@ -8,41 +8,41 @@ from math import prod
 
 
 def find_trees_for_slope(grid, i=1, j=3):
-	"""
-	Find the number of trees for a given slope.
+    """
+    Find the number of trees for a given slope.
 
-	grid : list of list of int
-		The skiiing course grid
-	i : int
-		The i of the slope
-	j : int
-		The j of the slope
-	"""
-	n_rows = len(grid) - 1
-	i_row, j_col, trees = 0, 0, 0
-	while i_row <= n_rows:
+    grid : list of list of int
+        The skiiing course grid
+    i : int
+        The i of the slope
+    j : int
+        The j of the slope
+    """
+    n_rows = len(grid) - 1
+    i_row, j_col, trees = 0, 0, 0
+    while i_row <= n_rows:
 
-		is_tree = int(grid[i_row][j_col] == 1)
-		trees += is_tree
+        is_tree = int(grid[i_row][j_col] == 1)
+        trees += is_tree
 
-		i_row += i
-		j_col += j
+        i_row += i
+        j_col += j
 
-	return trees
+    return trees
 
 
 if __name__ == '__main__':
 
-	with open('day3.txt') as fb:
-		data = [line.strip() for line in fb.readlines() if line]
-		grid = [[0 if cell == '.' else 1 for cell in row] * len(data)
-				for row in data]
+    with open('day3.txt') as fb:
+        data = [line.strip() for line in fb.readlines() if line]
+        grid = [[0 if cell == '.' else 1 for cell in row] * len(data)
+                for row in data]
 
-	# problem 1
-	print('Problem 1 Solution: ')
-	print(find_trees_for_slope(grid), '\n')
+    # problem 1
+    print('Problem 1 Solution: ')
+    print(find_trees_for_slope(grid), '\n')
 
-	# problem 2
-	slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
-	print('Problem 2 Solution: ')
-	print(prod(find_trees_for_slope(grid, i=i, j=j) for i, j in slopes), '\n')
+    # problem 2
+    slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+    print('Problem 2 Solution: ')
+    print(prod(find_trees_for_slope(grid, i=i, j=j) for i, j in slopes), '\n')
